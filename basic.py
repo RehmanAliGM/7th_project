2071cf0c4df4d2dc82970ee4cd62b31c32a4a04d
RehmanAliGM <gmworksheet294@gmail.com>
2023-02-09 19:39:24 +0500

    this is basic for practice only

---
+++ basic.py
@@ -0,0 +1,13 @@
+print("Hello,World!")
+print("Hello,World!") #This is a comment
+x=10
+y="this is string"
+print(x,y)
+x=11
+x="eleven"
+print(x) #this print command print only string
+#syntx to assign multiple veriables at single time
+x,y,z= "red","blue","green"
+print(x)
+mylist = ["apple", "banana", "cherry"]
+
